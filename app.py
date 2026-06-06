88f7d5c55531d6b5719c28c1040a524527c63533
naveenkumarr7979 <naveenkumarr253@gmail.com>
2026-06-06 13:14:58 +0530

    python learnings
    python learnings

---
+++ app.py
@@ -0,0 +1,36 @@
+MENU_PROPMT="\nEnter 'a' to add a movie , 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit : "
+movies=[]
+
+def add_movie():
+    title =input("Enter movie title: ")
+    director = input("Enter director: ")
+    year = input("Enter year: ")
+    movies.append({"title":title,"director":director,"year":year})
+
+def show_movies():
+    for moive in movies:
+        print(moive)
+
+def find_movie():
+    search_title=input("Enter movie title: ")
+    for movie in movies:
+        if movie['title'] == search_title:
+            print(movie)
+user_options={  #first class function impl
+    "a":add_movie,
+    "l":show_movies,
+    "f":find_movie
+}
+
+def menu():
+    selection=input(MENU_PROPMT)
+    while selection !='q':
+        if selection in user_options:
+            selected_option=user_options[selection]
+            selected_option()
+        else:
+            print('unknown command. please try again')
+
+        selection = input(MENU_PROPMT)
+
+menu()
